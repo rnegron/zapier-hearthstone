ceeefb31e57ae8c670e65546dd49b7a19aebcc29
Raúl Negrón <raul.negron@zapier.com>
2023-06-30 13:44:48 -0400

    add support for more fields

--- test/searches/card.test.js
+++ test/searches/card.test.js
@@ -1,20 +1,33 @@
-const zapier = require('zapier-platform-core');
-const nock = require('nock');
+const zapier = require("zapier-platform-core");
+const nock = require("nock");
 
 // Use this to make test calls into your app:
-const App = require('../../index');
+const App = require("../../index");
 const appTester = zapier.createAppTester(App);
+
+const { HEARTHSTONE_API_URL } = require("../../constants");
+
+const murlocFixture = require("../fixtures/textFilterMurlocManaCost8.json");
 
 // read the `.env` file into the environment, if available
 zapier.tools.env.inject();
 
+describe("searches.card", () => {
+  it("should run", async () => {
+    const bundle = {
+      inputData: { textFilter: "Murloc", locale: "en_US", class: "neutral", manaCost: 8 },
+    };
 
-describe('searches.card', () => {
-  it('should run', async () => {
-    const bundle = { inputData: {} };
+    nock(HEARTHSTONE_API_URL)
+      .get("/cards")
+      .query(bundle.inputData)
+      .reply(200, murlocFixture);
 
-    const results = await appTester(App.searches.card.operation.perform, bundle);
+    const results = await appTester(
+      App.searches.card.operation.perform,
+      bundle
+    );
     expect(results).toBeDefined();
-    // TODO: add more assertions
+    expect(results.id).toBe(1);
   });
 });
